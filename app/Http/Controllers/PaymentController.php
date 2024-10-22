fe28210f4e9f3f7e4bb344e26091c3bb2d711316
Intan Nuzulina <intannuzulina@gmail.com>
2024-10-22 16:45:02 +0700

    change transaction_status = settlement

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -55,7 +55,7 @@ class PaymentController extends Controller
         $hashed = hash('sha256', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         
         if ($request->signature_key) {
-            if($request->transaction_status == 'capture') {
+            if($request->transaction_status == 'settlement') {
                 $order = Order::where('order_id', $request->order_id)->first();
                 $order->status = 'paid';
                 $order->save();
