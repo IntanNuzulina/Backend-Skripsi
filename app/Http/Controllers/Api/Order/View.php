a30624b4e1ead0492f719aa19ae61225f9568127
Intan Nuzulina <intannuzulina@gmail.com>
2024-10-22 18:02:03 +0700

    edit order view

--- app/Http/Controllers/Api/Order/View.php
+++ app/Http/Controllers/Api/Order/View.php
@@ -12,26 +12,12 @@ class View extends Controller
     public function __invoke()
     {
         $order = Order::all();
-
-        if ($order->count() == 0) {
-            return response()->json([
-                'message' => 'Tidak ada data'
-            ], 404);
-        } else {
-            return OrderResource::collection($order);
-        }
+        return OrderResource::collection($order);
     }
 
     public function orderUser() 
     {
         $order = Order::where('user_id', auth()->user()->id)->get();
-
-        if ($order->count() == 0) {
-            return response()->json([
-                'message' => 'Tidak ada data'
-            ], 404);
-        } else {
-            return OrderResource::collection($order);
-        }
+        return OrderResource::collection($order);
     }
 }
